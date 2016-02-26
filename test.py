69bb8cbae7122a01c80dba00a629785a0ad2aca2
dlb70 <dlb70@uclive.ac.nz>
2016-02-27 11:02:54 +1300

    modified message

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(("127.0.0.1",10099))
-s.sendto(bytes("hello",'utf-8'),("127.0.0.1",10012))
+s.sendto(bytes("Hello!",'utf-8'),("127.0.0.1",10012))
 s.close()

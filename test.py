280e3a3bc7042bf0b106170540175a227539d76e
David Barclay <dlb70@uclive.ac.nz>
2016-02-28 13:13:21 +1300

    deleted init.sh and test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(("127.0.0.1",10099))
-s.sendto(bytes("Hello!",'utf-8'),("127.0.0.1",10012))
-s.close()

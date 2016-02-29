e4c45cc350dc6fca621819d7e4f3b2c43700452d
David Barclay <dlb70@uclive.ac.nz>
2016-02-29 14:27:53 +1300

    Corrected test configurations, added geometry to test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 ### Initialization script for RIP simulation ###
 
+GEOM="--geometry=50x26"
+
 for i in 1 2 3; do
   echo $i;
-  gnome-terminal -e "python3 rip.py config/$i.cfg" -t "Router $i" &
+  gnome-terminal $GEOM -e "python3 rip.py $i.cfg" -t "Router $i" &
   sleep 0.2
 done

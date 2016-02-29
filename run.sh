969ff62fb2d6bd9be2a40bf76e062df95a00e3da
David Barclay <dlb70@uclive.ac.nz>
2016-02-29 14:19:08 +1300

    Added custom geometry to run.sh for 1680x1050p displays

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 ### Initialization script for RIP simulation ###
 
+GEOM="--geometry=50x26"
+
 for i in {1..7}; do
   echo $i;
-  gnome-terminal -e "python3 rip.py config/$i.cfg" -t "Router $i" &
+  gnome-terminal $GEOM -e "python3 rip.py config/$i.cfg" -t "Router $i" &
   sleep 0.2
 done
